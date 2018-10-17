00fe24424dabd2c960b2b91a6d58c5e9e83f6532
ybxxszl <1062837400@qq.com>
2018-10-17 21:29:44 +0800

    token

---
+++ src/main/java/com/wjy/TokenApplication.java
@@ -0,0 +1,12 @@
+package com.wjy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenApplication.class, args);
+	}
+}
