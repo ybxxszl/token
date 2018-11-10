27e6423ae8da733909177698f50d690700a1fcf5
ybxxszl <1062837400@qq.com>
2018-11-10 14:59:08 +0800

    token

--- src/main/java/com/wjy/TokenApplication.java
+++ src/main/java/com/wjy/TokenApplication.java
@@ -2,9 +2,16 @@ package com.wjy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TokenApplication {
+public class TokenApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(TokenApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TokenApplication.class, args);
